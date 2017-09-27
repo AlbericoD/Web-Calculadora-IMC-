e5fe0204e0791302d1f91db660d297b05b060ead
AlbericoD <a.diasbarreto@icloud.com>
2017-09-27 15:29:33 -0300

    Adcionado calculo para toda tabelal

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,34 @@
 var titulo = document.querySelector(".titulo");
-//console.log(titulo.textContent);
-//console.log(titulo);
 titulo.textContent = "Calculos Testes";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
-var imc = paciente.querySelector(".info-imc");
-var validacao_peso = (peso <= 0 || peso >= 300);
-var validacao_altura = (altura <= 0 || altura >= 3.00);
-var calc_ok = true; 
+var pacientes = document.querySelectorAll(".paciente");
+//console.log(pacientes);
+for (var index = 0; index < pacientes.length; index++) {
+ 
+   var peso = pacientes[index].querySelector(".info-peso").textContent;
+   var altura = pacientes[index].querySelector(".info-altura").textContent;
+   var imc = pacientes[index].querySelector(".info-imc");
 
-if (validacao_peso) {
-   console.log("teste dados do peso inválido");
-   imc.textContent = "Peso inválido";
-   calc_ok = false;
-}else if(validacao_altura){
-   console.log("teste dados da altura inválido");
-   imc.textContent = "Altura inválida";
-   calc_ok = false;
-}
-if (calc_ok) {
+   var validacao_peso = (peso <= 0 || peso >= 300);
+   var validacao_altura = (altura <= 0 || altura >= 3.00);
+   var calc_ok = true; 
    
-   imc.textContent = peso / (altura*altura);
-   console.log(paciente);
-   console.log("Validação ok!");
+   if (validacao_peso) {
+      console.log("teste dados do peso inválido");
+      imc.textContent = "Peso inválido";
+      calc_ok = false;
+   }else if(validacao_altura){
+      console.log("teste dados da altura inválido");
+      imc.textContent = "Altura inválida";
+      calc_ok = false;
+   }
+   if (calc_ok) {
+      
+     var  campo_imc = peso / (altura*altura);
+      imc.textContent = campo_imc.toFixed(2);
+      console.log(pacientes);
+      console.log("Validação ok!");
+   }
 }
 
 
