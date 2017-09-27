79e0989f04c09aa1b4bcd12067f01d991b7cdb67
AlbericoD <a.diasbarreto@icloud.com>
2017-09-27 16:05:08 -0300

    Alterado Js e Css
    Adicionado cores em erros de validação, ativados por js
    adicionado 2 classes no css para manipular as cores pelo js;

--- js/principal.js
+++ js/principal.js
@@ -1,13 +1,14 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Calculos Testes";
 
-var pacientes = document.querySelectorAll(".paciente");
-//console.log(pacientes);
+var pacientes = document.querySelectorAll(".paciente"); //console.log(pacientes);
 for (var index = 0; index < pacientes.length; index++) {
  
-   var peso = pacientes[index].querySelector(".info-peso").textContent;
-   var altura = pacientes[index].querySelector(".info-altura").textContent;
-   var imc = pacientes[index].querySelector(".info-imc");
+   var paciente = pacientes[index];
+
+   var peso = paciente.querySelector(".info-peso").textContent;
+   var altura = paciente.querySelector(".info-altura").textContent;
+   var imc = paciente.querySelector(".info-imc");
 
    var validacao_peso = (peso <= 0 || peso >= 300);
    var validacao_altura = (altura <= 0 || altura >= 3.00);
@@ -16,16 +17,19 @@ for (var index = 0; index < pacientes.length; index++) {
    if (validacao_peso) {
       console.log("teste dados do peso inválido");
       imc.textContent = "Peso inválido";
+      paciente.classList.add("paciente-invalido");
       calc_ok = false;
    }else if(validacao_altura){
       console.log("teste dados da altura inválido");
       imc.textContent = "Altura inválida";
+      paciente.classList.add("paciente-invalido");
       calc_ok = false;
    }
    if (calc_ok) {
       
      var  campo_imc = peso / (altura*altura);
       imc.textContent = campo_imc.toFixed(2);
+      imc.classList.add("paciente-valido");
       console.log(pacientes);
       console.log("Validação ok!");
    }
